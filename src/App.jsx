51e41545bd432bc27de145355ff810c5833d85e9
amitlzkpa <amit.lzkpa@gmail.com>
2024-11-27 04:52:38 +0530

    Add mantine AppShell

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,55 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { MantineProvider } from '@mantine/core';
+
+import { AppShell, Burger, Group, MantineProvider, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import Dev from "./views/Dev";
 
+export function App() {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dev />,
-  },
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dev />,
+    },
+  ]);
 
-function App() {
   return (
     <MantineProvider>
-      <RouterProvider router={router} />
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+            Kuonsensus
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <RouterProvider router={router} />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
