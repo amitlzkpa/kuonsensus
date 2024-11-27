6f62f2d9362746f4232c71999770a8eeecfc0764
amitlzkpa <amit.lzkpa@gmail.com>
2024-11-27 22:03:03 +0530

    Set up templates to create boards

--- src/views/Board.jsx
+++ src/views/Board.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { Text } from '@mantine/core';
 
+const boardTemplate = {
+  boardId: "brd_0001",
+  boardName: "Sample Board",
+  boardDescription: "This is a sample board",
+  proposalPrompt: "",
+  proposalDatabank: [],
+  currStakeholders: [],
+  currSideEffects: [],
+  storedConversations: [],
+};
+
+const conversationHistoryTemplate = {
+  conversationId: "",
+  conversationMessages: [],
+  conversationStakeholders: [],
+  conversationParticipants: [],
+  currConversationOutline: [],
+};
+
 const Board = () => {
   return (
     <div>
