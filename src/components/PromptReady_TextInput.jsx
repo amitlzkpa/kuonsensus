2650c564330c10556772a133189752e251ad054f
amitlzkpa <amit.lzkpa@gmail.com>
2024-12-01 05:26:38 +0530

    Add PromptReady_TextInput component for user input functionality

---
+++ src/components/PromptReady_TextInput.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Flex, Input, TextInput } from '@mantine/core';
+
+export const PromptReady_TextInput = (props) => {
+  return (
+    <div style={{ position: "relative", height: props.height ?? "2.25rem", resize: "vertical" }}>
+      <Input
+        style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}
+      />
+      <div style={{ position: "absolute", bottom: 0, right: 0 }}>
+        <Button>P</Button>
+      </div>
+    </div>
+  );
+}
+
+export default PromptReady_TextInput;
