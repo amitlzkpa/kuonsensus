bfd98f303b639f419911ad8991190c018fcb595a
amitlzkpa <amit.lzkpa@gmail.com>
2024-11-30 17:36:17 +0530

    Enhance Kuon3D_StakeHolder component with hover effects and update edges color

--- src/components/Kuon3D_StakeHolder.jsx
+++ src/components/Kuon3D_StakeHolder.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { Edges } from '@react-three/drei';
 
-export const Kuon3D_Stakeholder = () => {
+export const Kuon3D_StakeHolder = () => {
+
+  const [hovered, hover] = useState(false);
+
   return (
-    <mesh position={[0, 0, 0]} receiveShadow>
+    <mesh
+      position={[0, 0, 0]}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+      receiveShadow
+    >
       <cylinderGeometry attach="geometry" args={[4, 4, 1, 128]} />
       <meshStandardMaterial attach="material" color={"#DEDEDE"} />
-      <Edges linewidth={1} threshold={15} color={"black"} />
+      <Edges linewidth={hovered ? 4 : 2} threshold={15} color={hovered ? "#c02040" : "black"} />
     </mesh>
   );
 };
 
-export default Kuon3D_Stakeholder;
+export default Kuon3D_StakeHolder;
