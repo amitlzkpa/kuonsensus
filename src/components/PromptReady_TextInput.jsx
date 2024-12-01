ab01ebfbef5e13a29a1431675ec4149293bbb6e1
amitlzkpa <amit.lzkpa@gmail.com>
2024-12-01 14:08:20 +0530

    Add pen and debouced update to input component

--- src/components/PromptReady_TextInput.jsx
+++ src/components/PromptReady_TextInput.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useDebounce } from "@uidotdev/usehooks";
+import { FaPen } from 'react-icons/fa';
 import { Button, Flex, Input, TextInput } from '@mantine/core';
 
 export const PromptReady_TextInput = (props) => {
+
+  const [inputValue, setInputValue] = useState(props.inputProps.value);
+  const debouncedInputValue = useDebounce(inputValue, 500);
+
+  useEffect(() => {
+    if (props?.onChange_debounced) {
+      props?.onChange_debounced(debouncedInputValue);
+    }
+  }, [debouncedInputValue, props]);
+
   return (
     <div style={{ position: "relative", height: props.height ?? "2.25rem", resize: "vertical" }}>
       <Input
         style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}
         {...props.inputProps}
       />
-      <div style={{ position: "absolute", bottom: 0, right: 0 }}>
-        <Button>P</Button>
+      <div style={{ position: "absolute", top: "15%", right: 15, cursor: "pointer" }}>
+        <FaPen size="0.6rem" color="primary" />
       </div>
     </div>
   );
