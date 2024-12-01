aba170df29f3acda139abd5dc08c1206cb955d66
amitlzkpa <amit.lzkpa@gmail.com>
2024-12-01 16:05:58 +0530

    Integrate useLLMRef hook into PromptReady_TextInput for enhanced text generation functionality

--- src/components/PromptReady_TextInput.jsx
+++ src/components/PromptReady_TextInput.jsx
@@ -3,24 +3,74 @@ import { useDebounce } from "@uidotdev/usehooks";
 import { FaPen } from 'react-icons/fa';
 import { Button, Flex, Input, TextInput } from '@mantine/core';
 
-export const PromptReady_TextInput = (props) => {
+import { useLLMRef } from "../hooks/llmRef";
 
-  const [inputValue, setInputValue] = useState(props.inputProps.value);
+export const PromptReady_TextInput = ({
+  height = "2.25rem",
+  promptBase = "",
+  promptSamples = "",
+  onChange_debounced,
+  onGeneratedValueChange,
+  inputProps = {},
+  ...props
+}) => {
+
+  const llmRef = useLLMRef();
+
+  const [inputValue, setInputValue] = useState(inputProps?.value ?? "");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [generatedValue, setGeneratedValue] = useState("");
   const debouncedInputValue = useDebounce(inputValue, 500);
 
   useEffect(() => {
-    if (props?.onChange_debounced) {
-      props?.onChange_debounced(debouncedInputValue);
+    if (onChange_debounced) {
+      onChange_debounced(debouncedInputValue);
     }
-  }, [debouncedInputValue, props]);
+  }, [debouncedInputValue, onChange_debounced]);
+
+  const handleGenerationClick = async () => {
+    if (llmRef.current) {
+      try {
+        setIsGenerating(true);
+        setGeneratedValue("");
+        const inputGenPromptText = [
+          "## Instructions:",
+          promptBase ?? "",
+          "",
+          "## Samples:",
+          promptSamples ?? "",
+          "",
+          "## Strict Instructions:",
+          "Return only unformatted response text in English."
+        ].join("\n");
+        console.log(inputGenPromptText);
+        const generatedText = await llmRef.current?.prompt(inputGenPromptText);
+        console.log(generatedText);
+        setInputValue(generatedText);
+        setGeneratedValue(generatedText);
+        if (onGeneratedValueChange) {
+          onGeneratedValueChange(generatedText);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+      finally {
+        setIsGenerating(false);
+      }
+    }
+  };
 
   return (
-    <div style={{ position: "relative", height: props.height ?? "2.25rem", resize: "vertical" }}>
+    <div style={{ position: "relative", height }}>
       <Input
         style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}
-        {...props.inputProps}
+        {...inputProps}
+        disabled={isGenerating}
       />
-      <div style={{ position: "absolute", top: "15%", right: 15, cursor: "pointer" }}>
+      <div
+        onClick={handleGenerationClick}
+        style={{ position: "absolute", top: "15%", right: 15, cursor: "pointer" }}
+      >
         <FaPen size="0.6rem" color="primary" />
       </div>
     </div>
