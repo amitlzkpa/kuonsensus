719dd09cea32ab27847a9d98892e984db5a877de
amitlzkpa <amit.lzkpa@gmail.com>
2024-12-01 05:39:43 +0530

    Add inputProps support to PromptReady_TextInput component

--- src/components/PromptReady_TextInput.jsx
+++ src/components/PromptReady_TextInput.jsx
@@ -6,6 +6,7 @@ export const PromptReady_TextInput = (props) => {
     <div style={{ position: "relative", height: props.height ?? "2.25rem", resize: "vertical" }}>
       <Input
         style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}
+        {...props.inputProps}
       />
       <div style={{ position: "absolute", bottom: 0, right: 0 }}>
         <Button>P</Button>
