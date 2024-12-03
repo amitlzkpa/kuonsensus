07cd7ab5565b7735887458733c838a82606e57a7
amitlzkpa <amit.lzkpa@gmail.com>
2024-12-04 01:16:33 +0530

    Add tiptap and show generated article

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '@mantine/tiptap/styles.css';
 import './index.css';
 import React, { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
